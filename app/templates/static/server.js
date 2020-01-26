27ef0efb7f519ce9486113bdab4761193084ed44
jacklrs <github@rushsoft.de>
2020-01-26 19:44:35 +0100

    cleanup

--- app/templates/static/server.js
+++ app/templates/static/server.js
@@ -1,26 +1,12 @@
 
 //for local testing and review only!
+process.env.NODE_ENV = 'development'
+//for local testing and review only!
 const composition = require('@pubcore/node-composition').default,
 	express = require('express'),
 	app = express(),
 	context_path = '/test',
 	config = {components:{'./js/index':{public:false, context_path}}}
 
-//fake access token
-const	cookie = require('cookie')
-app.use(`${context_path}/login`, (req, res, next) => {
-	res.setHeader(
-		'Set-Cookie',
-		cookie.serialize('accessToken', '0101010101', {
-			httpOnly: true,
-			path: '/',
-			secure: false,//insecure !; for local demo only!
-		})
-	)
-	next()
-})
-//only needed in this demo mode
-app.use(context_path, express.static('./htdocs'))
-
-app.use('/', composition(config, id => require(id)))
+app.use('/', composition(config, require))
 app.listen(3030)
